c759fed87ae17fcf1d44a2a6f88719bbb7ea595b
yuanfang-dev <yuanfang-dev>
2023-10-07 08:47:11 +0800

    feat: add poppins font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
+import { Poppins } from 'next/font/google'
 import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
 
+const poppins = Poppins({ weight: '400', subsets: ['latin-ext'] })
+
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 };
 
 export default api.withTRPC(MyApp);
